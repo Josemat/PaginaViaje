59ceed1e0aa43d2cbee068db982591dfaffc5a49
Emmanuel <emmatulian@gmail.com>
2022-04-12 10:19:25 -0300

    primer commit

---
+++ controllers/paginacontroller.js
@@ -0,0 +1,78 @@
+import { Viaje } from "../models/Viaje.js";
+import { Testimonial } from "../models/testimoniales.js";
+
+const paginaInicio =async (req,res)=>{//req(request) lo que enviamos | res(response) lo que express nos responde
+    //Consultar 3 viajes del modelo viaje
+    const promiseDB = [];
+    promiseDB.push(Viaje.findAll({ limit:3 }))
+    promiseDB.push(Testimonial.findAll({ limit: 3 }))
+    
+    try {
+        const resultado = await Promise.all(promiseDB)
+
+        res.render('inicio',{
+            pagina: 'Inicio',
+            clase: 'home',
+            viajes: resultado[0],
+            testimoniales: resultado[1]
+        });
+    } catch (error) {
+        console.log(error);
+    }
+    
+    
+    
+    }
+const paginaNosotros = (req,res)=>{//req(request) lo que enviamos | res(response) lo que express nos responde
+    res.render('nosotros',{
+        pagina: 'Nosotros'
+    });
+    }
+const paginaViajes = async (req,res)=>{
+    //Aca consultamos la DB
+    const viajes = await Viaje.findAll();
+    
+    res.render('viajes',{
+        pagina: 'Viajes',
+        viajes
+    });
+    }
+const paginaTestimoniales = async (req,res)=>{
+
+    try {
+        const testimoniales = await Testimonial.findAll();
+        res.render('testimoniales',{
+            pagina: 'Testimonios',
+            testimoniales
+        });
+    } catch (error) {
+        console.log(error);
+    }
+    
+    }
+
+
+    //Muestrra un viaje por su slug
+const paginaDetalleViajes = async (req, res)=>{
+    const {slug} = req.params
+    try {
+        const resultado = await Viaje.findOne({where : {slug}})
+        res.render('viaje',{
+            pagina:'Informacion Viaje',
+            resultado
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+
+    export {
+        paginaInicio,
+        paginaNosotros,
+        paginaViajes,
+        paginaTestimoniales,
+        paginaDetalleViajes
+    }
---
+++ index.js
@@ -0,0 +1,46 @@
+// Common JS, no es propia de Javascript
+// const express = require('express');
+// La version de imports
+import express from 'express';
+import router from  './routes/index.js'
+import db from './config/db.js';
+import dotenv from 'dotenv';
+dotenv.config({path:"variables.env"});
+
+
+const app = express();
+
+// Conectar Base de datos
+db.authenticate()
+        .then(()=>{console.log('Base de datos conectada...')})
+        .catch((error)=>console.log(error));
+
+
+// Definir puerto
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 4000;
+//Habilitar PUG
+app.set('view engine','pug')
+
+// Creando nuestro propio middleware
+// Obtener el año actual
+app.use((req, res, next)=>{
+    res.locals.unaVariable = 'Todos los derechos reservados '
+    const newYear = new Date();
+    res.locals.actualYear = newYear.getFullYear()
+    res.locals.nombreSitio = 'Agencia de viajes'
+    next() //con next o return next obligamos a que siga ejecutando
+})
+
+//Agregar body parser para leer los datos del form
+app.use(express.urlencoded({extended: true}))
+
+// Definir la carpeta publica
+app.use(express.static('public'))
+// Agregar router
+app.use('/', router)
+
+
+app.listen(port,host,()=>{
+    console.log(`El servidor está funcionando en el host: ${host} y el puerto ${port}`)
+})
